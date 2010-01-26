4ba55637ed8a88c989d6f10f4059ee007bbc0a77
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-01-26 22:48:47 +0000

    Add Hailo::Storage::Pg to store in PostgreSQL
    Populate it like this:
    
        dropdb failo
        createdb -E UTF8 failo
        SPAWN_TABLES=1 perl -Ilib script/hailo -S Pg -t megahal.trn -b failo
        perl -Ilib script/hailo -S Pg -b failo -r "foo"
    
    The SPAWN_TABLES environmental variable is a hack because I couldn't
    figure out how to check whether a failo database had been created but
    didn't have any schemas yet.

---
+++ lib/Hailo/Storage/Pg.pm
@@ -0,0 +1,97 @@
+package Hailo::Storage::Pg;
+use 5.10.0;
+use Moose;
+
+extends 'Hailo::Storage::SQLite';
+
+our $VERSION = '0.01';
+
+sub _build__dbh {
+    my ($self) = @_;
+
+    DBI->connect(
+        "dbi:Pg:dbname=".$self->file,
+        '',
+        '', 
+        {
+            pg_enable_utf8 => 1,
+            RaiseError => 1,
+        },
+    );
+}
+
+sub _exists_db {
+    my ($self) = @_;
+
+    # Hack!
+    not exists $ENV{SPAWN_TABLES};
+}
+
+sub start_training {
+    my ($self) = @_;
+
+    # start a transaction
+    $self->_dbh->begin_work;
+
+    return;
+}
+
+sub _last_expr_rowid  { shift->_dbh->selectrow_array('SELECT expr_id  FROM expr  ORDER BY expr_id  DESC LIMIT 1') }
+sub _last_token_rowid { shift->_dbh->selectrow_array('SELECT token_id FROM token ORDER BY token_id DESC LIMIT 1') }
+
+__PACKAGE__->meta->make_immutable;
+
+=encoding utf8
+
+=head1 NAME
+
+Hailo::Storage::Pg - A storage backend for L<Hailo|Hailo> using
+L<DBD::Pg|DBD::Pg>
+
+=head1 DESCRIPTION
+
+This backend maintains information in a PostgreSQL database, the same
+caveats apply to it as
+L<Hailo::Storage::SQLite|Hailo::Storage::SQLite>.
+
+=head1 AUTHOR
+
+E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason.
+
+This program is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__DATA__
+__[ table_token ]__
+CREATE TABLE token (
+    token_id SERIAL UNIQUE,
+    text     TEXT NOT NULL
+);
+__[ table_expr ]__
+CREATE TABLE expr (
+    expr_id   SERIAL UNIQUE,
+    expr_text TEXT NOT NULL UNIQUE
+);
+[% FOREACH i IN orders %]
+ALTER TABLE expr ADD token[% i %]_id INTEGER REFERENCES token (token_id);
+[% END %]
+__[ table_next_token ]__
+CREATE TABLE next_token (
+    pos_token_id SERIAL UNIQUE,
+    expr_id      INTEGER NOT NULL REFERENCES  expr (expr_id),
+    token_id     INTEGER NOT NULL REFERENCES token (token_id),
+    count        INTEGER NOT NULL
+);
+__[ table_prev_token ]__
+CREATE TABLE prev_token (
+    pos_token_id SERIAL UNIQUE,
+    expr_id      INTEGER NOT NULL REFERENCES expr (expr_id),
+    token_id     INTEGER NOT NULL REFERENCES token (token_id),
+    count        INTEGER NOT NULL
+);
