8c8bbf337dd2b18807d92ac33797dc50d18b1272
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-01-28 23:03:30 +0000

    Add some POD

--- lib/Hailo/Role/Storage/SQL.pm
+++ lib/Hailo/Role/Storage/SQL.pm
@@ -15,3 +15,22 @@ has _sth => (
 );
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+Hailo::Role::Storage::SQL - A an extension of L<Hailo::Role::Storage> for SQL backends
+
+=head1 AUTHOR
+
+E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason.
+
+This program is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
