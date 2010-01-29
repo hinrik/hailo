hinrik/hailo

commit 13cb906fffd5b24a3d3e945da5f222ceb8b59eb7

Check for definedness of $self->brain in Hailo::Storage::*
This catches:

    $ perl -Ilib script/hailo --train /tmp/fooblaoeu
    Use of uninitialized value $db in concatenation (.) or string at lib/Hailo/Storage/SQL.pm line 52.
    Use of uninitialized value in -s at lib/Hailo/Storage/SQLite.pm line 31.

lib/Hailo/Storage/SQLite.pm
@@ -28,7 +28,7 @@ after stop_training => sub {
 
 sub _exists_db {
     my ($self) = @_;
-    return -s $self->brain;
+    return defined $self->brain and -s $self->brain;
 }
 
 __PACKAGE__->meta->make_immutable;

lib/Hailo/Storage/SQL.pm
@@ -47,7 +47,7 @@ sub _build_dbi_options {
     my ($self) = @_;
     my $dbd = $self->dbd;
     my $dbd_options = $self->dbd_options;
-    my $db = $self->brain;
+    my $db = $self->brain // '';
 
     my @options = (
         "dbi:$dbd:dbname=$db",

commit 9b2f1f75e3de24f6eb8ce1749294f385296d630c

Oops, logic error in 13cb906fff broke the sqlite backend

lib/Hailo/Storage/SQLite.pm
@@ -28,7 +28,9 @@ after stop_training => sub {
 
 sub _exists_db {
     my ($self) = @_;
-    return defined $self->brain and -s $self->brain;
+    my $brain = $self->brain;
+    return unless defined $self->brain;
+    return -s $self->brain;
 }
 
 __PACKAGE__->meta->make_immutable;