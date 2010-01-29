2b983557f52ca1f9ca031228088c89898005ff08
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-01-29 01:36:39 +0000

    Fix spelling error in Hailo's POD

--- lib/Hailo.pm
+++ lib/Hailo.pm
@@ -387,7 +387,7 @@ Hailo - A pluggable Markov engine analogous to MegaHAL
 Hailo is a fast and lightweight markov engine intended to replace
 L<AI::MegaHAL>. It has a lightweight L<Moose>-based core with
 pluggable L<storage|Hailo::Role::Storage>,
-L<tokenizer|Hailo::Role::Tokenizer>and L<engine|Hailo::Role::Engine>
+L<tokenizer|Hailo::Role::Tokenizer> and L<engine|Hailo::Role::Engine>
 backends.
 
 It's faster than MegaHAL and can handle huge brains easily with the
