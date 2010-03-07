1adfd87471380392da81f8fa8983da20eafd09fb
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-07 20:24:31 +0000

    Resolve the "run tests under Moose+Mouse" TODO via a custom Dist::Zilla plugin
    This requires ecb5c8ed49db2967ef1f43d74f2b6f02fe39aead from my dist-zilla branch

---
+++ utils/developer/Dist/Zilla/Plugin/HailoMakeMaker.pm
@@ -0,0 +1,26 @@
+package Dist::Zilla::Plugin::HailoMakeMaker;
+use Moose;
+
+extends 'Dist::Zilla::Plugin::MakeMaker';
+
+override _build_makemaker_template => sub {
+    my ($self) = @_;
+    my $template = super();
+
+    $template .= <<'TEMPLATE';
+package MY;
+
+sub test {
+    my $inherited = shift->SUPER::test(@_);
+
+    # Run tests with Moose and Mouse
+    $inherited =~ s/^test_dynamic :: pure_all\n\t(.*?)\n/test_dynamic :: pure_all\n\tANY_MOOSE=Mouse $1\n\tANY_MOOSE=Moose $1\n/m;
+
+    return $inherited;
+}
+TEMPLATE
+
+    return $template;
+};
+
+__PACKAGE__->meta->make_immutable;
