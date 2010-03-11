a2e0efe47a7c9ea1611b3df4621ecd1a01559be3
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-11 14:33:59 +0000

    test ->stats() for all engines

--- t/lib/Hailo/Test.pm
+++ t/lib/Hailo/Test.pm
@@ -406,21 +406,40 @@ sub test_starcraft {
 
 sub test_all_plan {
     my ($self, $restriction) = @_;
-    my $storage = $self->storage;    
+    my $storage = $self->storage;
 
   SKIP: {
     my $ok = $self->spawn_storage();
 
     plan skip_all => "Skipping $storage tests, can't create storage" unless $ok;
     if (defined $restriction && $restriction eq 'known') {
-        plan(tests => 951);
+        plan(tests => 971);
         $self->test_known;
     }
     else {
-        plan(tests => 952);
+        plan(tests => 976);
         $self->test_all;
     }
   }
+}
+
+sub test_stats {
+    my ($self, $test_name) = @_;
+    state $last_token = 0;
+    state $last_expr = 0;
+    state $last_prev = 0;
+    state $last_next = 0;
+
+    my ($token, $expr, $prev, $next) = $self->hailo->stats();
+
+    cmp_ok($last_token, "<=", $token, "token count is <= since last time from $last_token to $token");
+    cmp_ok($last_expr, "<=", $expr, "expr count is <= since last time from $last_expr to $expr");
+    cmp_ok($last_prev, "<=", $prev, "prev count is <= since last time from $last_prev to $prev");
+    cmp_ok($last_next, "<=", $next, "next count is <= since last time from $last_next to $next");
+
+    ($last_token, $last_expr, $last_prev, $last_next) = ($token, $expr, $prev, $next);
+
+    return;
 }
 
 sub test_known {
@@ -428,6 +447,7 @@ sub test_known {
 
     for (all_tests_known()) {
         $self->$_;
+        $self->test_stats($_);
     }
 
     return;
@@ -438,6 +458,7 @@ sub test_all {
 
     for (all_tests()) {
         $self->$_;
+        $self->test_stats($_);
     }
 
     return;
