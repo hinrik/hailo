19329227ffbf7524d57375d7b6764f2ec9b00e14
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-14 14:33:37 +0000

    Manually adjust the gource options

--- utils/developer/gource-changes.sh
+++ utils/developer/gource-changes.sh
@@ -2,4 +2,16 @@
 
 # Run this in hailo's checked out directory
 
-gource --stop-at-end --output-ppm-stream - | ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 gource.mp4
+gource \
+    --stop-at-end \
+    --file-idle-time 9900 \
+    --elasticity 0.3 \
+    --auto-skip-seconds 1 \
+    --highlight-all-users \
+    --user-image-dir \
+    ~/gource-img \
+    --user-scale 0.8 \
+    --camera-mode track \
+    --output-ppm-stream ppm-stream.out
+
+pv ppm-stream.out | ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 hailo-gource.mp4
