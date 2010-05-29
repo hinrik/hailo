0591c70c5bed89b8d3ec2baa2b077aec167b863a
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-05-29 15:10:09 +0000

    Add built-in commands to hailo(1)'s ReadLine. Including help system

--- lib/Hailo/UI/ReadLine.pm
+++ lib/Hailo/UI/ReadLine.pm
@@ -10,6 +10,7 @@ BEGIN {
 use Encode 'decode';
 use Hailo;
 use Term::ReadLine;
+use Data::Dump 'dump';
 use namespace::clean -except => 'meta';
 
 with qw(Hailo::Role::Arguments
@@ -24,15 +25,142 @@ sub run {
     my ($self, $hailo) = @_;
     my $name = 'Hailo';
     my $term = Term::ReadLine->new($name);
+    my $command = qr[
+        ^
+        # A dot-prefix like in SQLite
+        \.
+        # We only have Hailo methods matching this
+        (?<method> [a-z_]+ )
+        # Optional arguments. These'll be passed to eval() before being
+        # passed to the method
+        \s*
+        (?: (?<arguments>.+) )?
+    $]x;
+
+    print $self->_intro;
 
     while (defined (my $line = $term->readline($name . '> '))) {
         $line = decode('utf8', $line);
-        my $answer = $hailo->learn_reply($line);
-        say $answer // "I don't know enough to answer you yet.";
+
+        given ($line) {
+            when (/$command/p) {
+                when ($+{method} eq 'help') {
+                    print $self->_help($hailo);
+                }
+                when ($+{method} ~~ [ qw< quit exit >]) {
+                    say $hailo->reply("Dave, this conversation can serve no purpose anymore. Goodbye.") // "Bye!";
+                    exit 0;
+                }
+                default {
+                    my $meth = $+{method};
+                    my @args = defined $+{arguments} ? eval $+{arguments} : ();
+
+                    local ($@, $!);
+                    eval {
+                        say dump $hailo->$meth(@args);
+                    };
+                    if (my $err = $@) {
+                        chomp $err;
+                        say STDERR "Failed on <<${^MATCH}>>: <<$err>>";
+                    }
+                }
+            }
+            default {
+                my $answer = $hailo->learn_reply($line);
+                say $answer // "I don't know enough to answer you yet.";
+            }
+        }
     }
     print "\n";
 
     return;
+}
+
+sub _intro {
+    my ($self) = @_;
+    my $intro = <<"INTRO";
+Welcome to the Hailo interactive shell
+Enter ".help" to show the built-in commands.
+Input that's not a command will be passed to Hailo to learn, and it'll
+reply back.
+INTRO
+    return $intro;
+}
+
+sub _help {
+    my ($self, $hailo) = @_;
+
+    my $include = qr/ ^ _go /x;
+    my $exclude = qr/
+        _
+       (?:
+           version
+         | order
+         | progress
+         | random_reply
+         | examples
+         | autosave
+         | brain
+         | class
+       )
+    $/x;
+
+    my @attr;
+    for my $attr ($hailo->meta->get_all_attributes) {
+        # Only get attributes that are valid command-line options
+        next unless $attr->name ~~ $include;
+
+        # We don't support changing these in mid-stream
+        next if $attr->name ~~ $exclude;
+
+        push @attr => {
+            name => do {
+                my $tmp = $attr->cmd_flag;
+                $tmp =~ tr/-/_/;
+                $tmp;
+            },
+            documentation => $attr->documentation,
+        };
+    }
+
+    push @attr => {
+        name => 'quit',
+        documentation => "Exit this chat session",
+    };
+
+    my $help = <<"HELP";
+These are the commands we know about:
+
+HELP
+
+    my @sorted = sort { $a->{name} cmp $b->{name} } @attr;
+    for my $cmd (@sorted) {
+        $help .= sprintf "    %-14s%s\n", '.'.$cmd->{name}, $cmd->{documentation};
+    }
+
+    $help .= <<"HELP";
+
+The commands are just method calls on a Hailo object. Any arguments to
+them will be passed through eval() used as method arguments. E.g.:
+
+    .train "/tmp/megahal.trn"
+    Trained from 350 lines in 0.54 seconds; 654.04 lines/s
+    ()
+
+Return values are printed with Data::Dump:
+
+    .stats
+    (1311, 2997, 3580, 3563)
+
+Any input not starting with "." will be passed through Hailo's
+learn_reply method:
+
+    Hailo> Help, mommy!
+    Really? I can't. It's an ethical thing.
+
+HELP
+
+    return $help;
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -50,7 +178,7 @@ This module is called internally by L<Hailo|Hailo>, it takes no options.
 A ReadLine interface will be presented when calling L<hailo> on the
 command-line with only a C<--brain> argument:
 
-    hailo --brain a-brain.brn
+    hailo --brain hailo.sqlite
 
 =head1 DESCRIPTION
 
--- t/command/ui/readline_chat.t
+++ t/command/ui/readline_chat.t
@@ -14,20 +14,48 @@ BEGIN {
 }
 use Test::Expect;
 
-plan tests => 5;
+plan tests => 25;
 
 expect_run(
-    command => "$^X bin/hailo -o 2 -b :memory:",
+    command => "$^X -Ilib bin/hailo -o 2 -b :memory:",
     prompt  => 'Hailo> ',
-    quit    => '',
+    quit    => '.quit',
 );
 
 expect_send('foo', "Shouldn't learn from this");
-expect_like(qr/I don't know enough/, "Don't get a reply");
+expect_like(qr/I don\'t know enough/, "Don't get a reply");
 
 expect_send('foo bar baz', 'Learn from the input');
 expect_like(qr/Foo bar baz\./, 'Get a relevant reply');
 
-# hailo(1) has no quit command. It only exits on EOF,
-# so we'll close it manually
-expect_handle->hard_close;
+expect_send('.stats', 'Ask for statistics');
+expect_like(qr/\(3, 2, 2, 2\)/, "Get statistics");
+
+expect_send('.learn "Moo Moo farm."', 'Learn the Moo Moo farm');
+expect_like(qr/\(\)/, "Learn a string");
+
+expect_send('.stats', 'Ask for statistics');
+expect_like(qr/\(6, 5, 5, 5\)/, "Get statistics");
+
+expect_send('.learn_reply "Moo Moo farm"', '.learn_reply');
+expect_like(qr/".*moo.*\."/i, "learn and reply");
+
+expect_send('.stats', 'Ask for statistics');
+expect_like(qr/\(6, 5, 5, 6\)/, "Get statistics");
+
+expect_send('.learn_reply hello', 'A bareword');
+expect_like(qr/Failed on.*undef input/, "Failed on a bareword");
+
+expect_send('.stats', 'Ask for statistics');
+expect_like(qr/\(6, 5, 5, 6\)/, "Get statistics");
+
+expect_send('.train "' . __FILE__ . '"', 'Train from this file: ' . __FILE__);
+expect_like(qr/Trained from/, "successful training");
+
+expect_send('.stats', 'Ask for statistics');
+expect_like(qr/\(169, 269, 302, 316\)/, "Get statistics");
+
+expect_send('.help', 'Ask for some help');
+expect_like(qr/The commands are just method calls/, "Get help from ReadLine");
+
+
