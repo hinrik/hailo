f07521bb1a175d5a58054fa32e54bebb4d0fc51d
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2011-04-30 15:53:04 +0000

    Shorten this

--- lib/Hailo/Storage/Schema.pm
+++ lib/Hailo/Storage/Schema.pm
@@ -117,22 +117,19 @@ sub sth {
         last_expr_rowid  => qq[SELECT id FROM expr ORDER BY id DESC LIMIT 1;],
         last_token_rowid => qq[SELECT id FROM token ORDER BY id DESC LIMIT 1;],
 
-        next_token_links => qq[SELECT SUM(count) FROM next_token WHERE expr_id = ?;],
-        prev_token_links => qq[SELECT SUM(count) FROM prev_token WHERE expr_id = ?;],
-        next_token_count => qq[SELECT count FROM next_token WHERE expr_id = ? AND token_id = ?;],
-        prev_token_count => qq[SELECT count FROM prev_token WHERE expr_id = ? AND token_id = ?;],
-        next_token_inc   => qq[UPDATE next_token SET count = count + 1 WHERE expr_id = ? AND token_id = ?],
-        prev_token_inc   => qq[UPDATE prev_token SET count = count + 1 WHERE expr_id = ? AND token_id = ?],
-        next_token_add   => qq[INSERT INTO next_token (expr_id, token_id, count) VALUES (?, ?, 1);],
-        prev_token_add   => qq[INSERT INTO prev_token (expr_id, token_id, count) VALUES (?, ?, 1);],
-        next_token_get   => qq[SELECT token_id, count FROM next_token WHERE expr_id = ?;],
-        prev_token_get   => qq[SELECT token_id, count FROM prev_token WHERE expr_id = ?;],
-
         token_count      => qq[SELECT count FROM token WHERE id = ?;],
 
         add_expr         => qq[INSERT INTO expr ($columns) VALUES ($ids)],
         expr_id          => qq[SELECT id FROM expr WHERE ] . join(' AND ', map { "token${_}_id = ?" } @orders),
     );
+
+    for my $table (qw(next_token prev_token)) {
+        $state{"${table}_links"} = qq[SELECT SUM(count) FROM $table WHERE expr_id = ?;],
+        $state{"${table}_count"} = qq[SELECT count FROM $table WHERE expr_id = ? AND token_id = ?;],
+        $state{"${table}_inc"}   = qq[UPDATE $table SET count = count + 1 WHERE expr_id = ? AND token_id = ?],
+        $state{"${table}_add"}   = qq[INSERT INTO $table (expr_id, token_id, count) VALUES (?, ?, 1);],
+        $state{"${table}_get"}   = qq[SELECT token_id, count FROM $table WHERE expr_id = ?;],
+    }
 
     for (@orders) {
         $state{"expr_by_token${_}_id"} = qq[SELECT * FROM expr WHERE token${_}_id = ? ORDER BY $q_rand LIMIT 1;];
